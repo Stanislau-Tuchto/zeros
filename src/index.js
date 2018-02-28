91a59c8ecae757988189ee264d203b5a58a38967
Stanislau-Tuchto <toshiro_mifune@mail.ru>
2018-02-28 23:23:18 +0300

    Trying to get count of factorials zeros brutal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let carry = 0;
+    let arr = [1];
+    let k = 0;
+
+    for (let i = 1; i <= number; i++) {
+        for (let j = 0; j <= k; j++) {
+            arr[j] = arr[j] * i + carry;
+            carry = parseInt(arr[j] / 10, 10);
+            arr[j] = arr[j] % 10;
+        }
+        while (carry) {
+            k++;
+            arr[k] = carry % 10;
+            carry = parseInt(carry / 10, 10);
+        }
+    }
+
+    console.log(arr.reverse().join(""));
 }
